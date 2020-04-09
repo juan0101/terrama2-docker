5f1882fc3a7f469d966001b25237b87004154119
Marcelo Pilatti <marcelo.silva979@gmail.com>
2020-04-09 17:00:10 -0300

    .

--- production/configure.sh
+++ production/configure.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sh ./configure-version.sh
+./configure-version.sh
 
-sh ./install-packages.sh
+./install-packages.sh
 
 sudo docker volume create terrama2_shared_vol
 
 sudo docker-compose -p terrama2 up -d
 
-sh ./config-nginx.sh
-sh ./config-postgres.sh
-sh ./config-report.sh
+./config-report.sh
+./config-postgres.sh
+./config-nginx.sh
