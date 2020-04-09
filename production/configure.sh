daa8572db5bfc93b8c49435b92a5aa7ada70b48a
Marcelo Pilatti <marcelo.silva979@gmail.com>
2020-04-09 14:37:29 -0300

    Changed scripts execution order

--- production/configure.sh
+++ production/configure.sh
@@ -4,10 +4,10 @@ sh ./configure-version.sh
 
 sh ./install-packages.sh
 
-sh ./config-nginx.sh
-sh ./config-postgres.sh
-sh ./config-report.sh
-
 sudo docker volume create terrama2_shared_vol
 
 sudo docker-compose -p terrama2 up -d
+
+sh ./config-nginx.sh
+sh ./config-postgres.sh
+sh ./config-report.sh
